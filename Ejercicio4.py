778f523973299998f904d12714926f10ba467dad
stive <stiven23ospina@gmail.com>
2022-08-14 17:59:16 -0500

    Ejericio4

---
+++ Ejercicio4.py
@@ -0,0 +1,9 @@
+numero1=int(input("Ingrese el primer número, por favor\n"))
+numero2=int(input("Ingrese el segundo número, por favor\n"))
+numero3=int(input("Ingrese el tercer número, por favor\n"))
+
+if (numero1+ numero2) == numero3:
+    print("IGUALES")
+else:
+    print("DISTINTOS")
+    
